a5f8e01c48555d28f0a030c94df8847290a7e8c4
GabrielMendesCosta <gabrielmendescst@gmail.com>
2023-10-26 13:00:14 -0300

    feat: add banner and text to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import styled from "styled-components"
 import EstilosGlobais from "./components/EstilosGlobais"
 import Cabecalho from "./components/Cabecalho"
 import BarraLateral from "./components/BarraLateral"
+import TituloEstilizado from "./components/Banner"
+import Banner from "./components/Banner"
+import bannerBackground from '../public/imagens/background.png'
 
 const FundoGradiente = styled.div`
   background: linear-gradient(175deg, #1A0433 4.16%, #04244F 48%, #154580 96.76%, #154580 96.76%);
@@ -16,6 +19,11 @@ function App() {
       <EstilosGlobais />
       <Cabecalho />
       <BarraLateral />
+      <TituloEstilizado />
+      <Banner
+        texto="A galeria mais completa de fotos do espaço!"
+        backgroundImage={bannerBackground}
+      />
     </FundoGradiente>
   )
 }
