88d8ca687bdd174cf46d4888962a22ae4b019f9e
GabrielMendesCosta <gabrielmendescst@gmail.com>
2023-10-27 11:58:48 -0300

    fix: now the selected page is highlighted in the sidebar

--- src/components/BarraLateral/index.jsx
+++ src/components/BarraLateral/index.jsx
@@ -1,37 +1,53 @@
 import styled from "styled-components"
-import ItemNavegação from "./itemNavegacao"
+import ItemNavegacao from "./itemNavegacao"
 
 const ListaEstilizada = styled.ul`
-    list-style:none;
+    list-style: none;
     padding: 0;
     margin: 0;
     width: 212px;
 `
 
 const BarraLateral = () => {
-    return(<>
+    return (
         <aside>
             <nav>
                 <ListaEstilizada>
-                    <ItemNavegação iconeAivo="/icones/home-ativo.png" iconeInativo="/icones/home-inativo.png">
-                        Início    
-                    </ItemNavegação>
-                    <ItemNavegação iconeAivo="/icones/mais-vistas-ativo.png" iconeInativo="/icones/mais-vistas-inativo.png">
-                        Mais vistas    
-                    </ItemNavegação>
-                    <ItemNavegação iconeAivo="/icones/mais-curtidas-ativo.png" iconeInativo="/icones/mais-curtidas-inativo.png">
-                        Mais curtidas    
-                    </ItemNavegação>
-                    <ItemNavegação iconeAivo="/icones/novas-ativo.png" iconeInativo="/icones/novas-inativo.png">
-                        Novas 
-                    </ItemNavegação>
-                    <ItemNavegação iconeAivo="/icones/surpreenda-me-ativo.png" iconeInativo="/icones/surpreenda-me-inativo.png">
-                        Surpreenda-me   
-                    </ItemNavegação>
+                    <ItemNavegacao 
+                        iconeAtivo="/icones/home-ativo.png" 
+                        iconeInativo="/icones/home-inativo.png"
+                        ativo={true}
+                    >
+                        Início
+                    </ItemNavegacao>
+                    <ItemNavegacao 
+                        iconeAtivo="/icones/mais-vistas-ativo.png" 
+                        iconeInativo="/icones/mais-vistas-inativo.png"
+                    >
+                        Mais vistas
+                    </ItemNavegacao>
+                    <ItemNavegacao 
+                        iconeAtivo="/icones/mais-curtidas-ativo.png" 
+                        iconeInativo="/icones/mais-curtidas-inativo.png"
+                    >
+                        Mais curtidas
+                    </ItemNavegacao>
+                    <ItemNavegacao 
+                        iconeAtivo="/icones/novas-ativo.png" 
+                        iconeInativo="/icones/novas-inativo.png"
+                    >
+                        Novas
+                    </ItemNavegacao>
+                    <ItemNavegacao 
+                        iconeAtivo="/icones/surpreenda-me-ativo.png" 
+                        iconeInativo="/icones/surpreenda-me-inativo.png"
+                    >
+                        Surpreenda-me
+                    </ItemNavegacao>
                 </ListaEstilizada>
             </nav>
         </aside>
-    </>)
+    )
 }
 
 export default BarraLateral
