f0b45ffe0b412bfb91238813a9b8946859214710
GabrielMendesCosta <gabrielmendescst@gmail.com>
2023-10-27 11:54:50 -0300

    feat: created the tags

--- src/components/Galeria/Tags/index.jsx
+++ src/components/Galeria/Tags/index.jsx
@@ -1,5 +1,9 @@
+import tags from "./tags.json"
+
 const Tags = () => {
     return <>
+        <p>Busque por tags:</p>
+        {tags.map(tag => <button key={tag.id}>{tag.titulo}</button>)}
     </>
 }
 
