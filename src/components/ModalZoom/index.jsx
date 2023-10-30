97f159cf4e484bda310fa08895720661d2b12903
Gabriel Mendes Costa <gabrielmendescst@gmail.com>
2023-10-30 13:49:47 -0300

    feat: created Modal to zoom in on photos

---
+++ src/components/ModalZoom/index.jsx
@@ -0,0 +1,12 @@
+const ModalZoom = () => {
+    return (<>
+        <dialog open>
+            <p>Teste 1 2 3</p>
+            <form method="dialog">
+                <button>OK</button>
+            </form>
+        </dialog>
+    </>)
+}
+
+export default ModalZoom
